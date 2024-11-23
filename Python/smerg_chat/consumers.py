5269b999ae632fe9d65485da5465f496849daf10
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-23 17:53:34 +0530

    Investryx

--- Python/smerg_chat/consumers.py
+++ Python/smerg_chat/consumers.py
@@ -40,8 +40,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         file_name = None
         if data.get('audio'):
             audio = self.decode_data(data.get('audio'))
-            file_name = data.get('fileName', f"audio_{datetime.now().strftime('%Y%m%d_%H%M%S')}.wav")
-        recieved, created, room_data = await self.save_message(data.get('roomId'), data.get('token'), data.get('message'), audio, file_name)
+        recieved, created, room_data = await self.save_message(data.get('roomId'), data.get('token'), data.get('message'), audio)
         response = {
             'message': data.get('message'),
             'audio': data.get('audio'),
@@ -86,18 +85,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Saving Message to Db
     @sync_to_async
-    def save_message(self, roomId, token, msg, audio, file_name):
+    def save_message(self, roomId, token, msg, audio):
         room = Room.objects.get(id=roomId)
         recieved = room.second_person if self.user.id == room.first_person.id else room.first_person
         chat = ChatMessage.objects.create(sended_by=self.user, sended_to=recieved, room=room, message=encrypt_message(msg))
-        if audio and file_name:
-            audio_content = ContentFile(audio)
-            chat.audio.save(
-                name=file_name,
-                content=audio_content,
-                save=False
-            )
-        chat.save()
+        if audio:
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+            filename = f'audio_{self.user.username}_{timestamp}.wav'
+            chat.audio.save(filename, ContentFile(audio))
         print(chat)
         created = chat.timestamp
         room.last_msg = encrypt_message(msg)
