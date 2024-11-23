2e39a58243c9d0563223d3ab469bfe3ff2c49c9b
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-23 14:54:28 +0530

    Investryx

--- Python/smerg_chat/views.py
+++ Python/smerg_chat/views.py
@@ -54,6 +54,8 @@ class Chat(APIView):
             if exists:
                 chats = [chat async for chat in ChatMessage.objects.filter(room__id = request.GET.get('roomId')).order_by('-id')] 
                 serialized_data = await serialize_data(chats, ChatSerial)
-                return Response(serialized_data)
+                room   = Room.objects.get(id = request.GET.get('roomId'))
+                number = room.second_person.username if user.id == room.first_person.id else room.first_person.username
+                return Response({'messages': serialized_data, 'number': number })
             return Response({'status':False,'message': 'User doesnot exist'})
         return Response({'status':False,'message': 'Token is not passed'})
