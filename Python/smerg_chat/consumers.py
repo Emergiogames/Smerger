bdf21c88d767f376db9dace0a61419229493d277
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-25 12:03:59 +0530

    Investryx

--- Python/smerg_chat/consumers.py
+++ Python/smerg_chat/consumers.py
@@ -92,14 +92,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
             filename = f'audio_{self.user.username}_{timestamp}.m4a'
             decoded_audio = asyncio.run(self.decode_data(audio))
             if not decoded_audio:
-                raise ValueError("Decoded audio data is empty")
+                print("Decoded audio data is empty")
                 
             # Save using Django's ContentFile
             audio_file = ContentFile(decoded_audio)
             chat.audio.save(filename, audio_file, save=True)
             full_path = os.path.join(settings.MEDIA_ROOT, chat.audio.name)
+            print(full_path)
             if not os.path.exists(full_path):
-                raise FileNotFoundError(f"File was not saved at {full_path}")
+                print(f"File was not saved at {full_path}")
         print(chat)
         created = chat.timestamp
         room.last_msg = encrypt_message(msg)
