6b3aa5d42677a5b5426e49bd817faa6cfd3b3028
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-26 11:49:32 +0530

    Invetryx

--- Python/smerg_chat/models.py
+++ Python/smerg_chat/models.py
@@ -17,8 +17,8 @@ class ChatMessage(models.Model):
     sended_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sended')
     sended_to = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='recieved')
     message = models.TextField(null=True, blank=True)
-    audio = models.FileField(upload_to="chat/records/", null=True, blank=True)
-    attachment = models.FileField(upload_to="chat/attachments/", null=True, blank=True)
+    audio = models.FileField(storage=MediaStorage(), upload_to="chat/records/", null=True, blank=True)
+    attachment = models.FileField(storage=MediaStorage(), upload_to="chat/attachments/", null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
