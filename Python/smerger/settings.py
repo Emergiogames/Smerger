0e0e7e0a6fe55893868ce78daf137c93501ee6c9
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-26 11:41:25 +0530

    Investryx

--- Python/smerger/settings.py
+++ Python/smerger/settings.py
@@ -208,9 +208,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA_URL='/media/'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = BASE_DIR/'uploads'
+MEDIA_ROOT = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
