3614faa037c1f9b2b294dee4fc92cf224037c37c
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-27 10:07:35 +0530

    Investryx

--- Python/smerger/settings.py
+++ Python/smerger/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='').split(',')
-# CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', default='').split(',')
-# CSRF_ALLOWED_ORIGINS = config('DJANGO_CSRF_ALLOWED_ORIGINS', default='').split(',')
-# CORS_ORIGINS_WHITELIST = config('DJANGO_CORS_ORIGINS_WHITELIST', default='').split(',')
+CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', default='').split(',')
+CSRF_ALLOWED_ORIGINS = config('DJANGO_CSRF_ALLOWED_ORIGINS', default='').split(',')
+CORS_ORIGINS_WHITELIST = config('DJANGO_CORS_ORIGINS_WHITELIST', default='').split(',')
 
 ONESIGNAL_APP_ID = config('ONESIGNAL_APP_ID', default='ONESIGNAL_APP_ID')
 ONESIGNAL_API_KEY = config('ONESIGNAL_API_KEY', default='ONESIGNAL_API_KEY')
