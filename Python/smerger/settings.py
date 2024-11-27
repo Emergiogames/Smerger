cda6ef4436e15fccd395b3c31586ae232f7dbe4b
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-27 11:57:19 +0530

    Investryx

--- Python/smerger/settings.py
+++ Python/smerger/settings.py
@@ -202,7 +202,7 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATIC_ROOT = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
+# MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_ROOT = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
