2018de6b911034f5464493b213c92b3f9e796529
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-28 14:42:42 +0530

    Investryc

--- Python/smerg_chat/models.py
+++ Python/smerg_chat/models.py
@@ -5,7 +5,7 @@ from smerg_app.models import *
 class Room(models.Model):
     first_person = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='first_person')
     second_person = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='second_person')
-    last_msg = models.TextField(null=True, blank=True)
+    last_msg = models.TextField(null=True, blank=True, db_collation='utf8_unicode_ci')
     updated = models.DateTimeField(auto_now=True)
     created_date = models.DateTimeField(auto_now=True)
 
@@ -16,7 +16,7 @@ class ChatMessage(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     sended_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sended')
     sended_to = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='recieved')
-    message = models.TextField(null=True, blank=True)
+    message = models.TextField(null=True, blank=True, db_collation='utf8_unicode_ci')
     audio = models.FileField(storage=MediaStorage(), upload_to="chat/records/", null=True, blank=True)
     duration = models.CharField(max_length=100, default='', null=True, blank=True)
     attachment = models.FileField(storage=MediaStorage(), upload_to="chat/attachments/", null=True, blank=True)
