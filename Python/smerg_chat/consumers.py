fbecb6faeb5af25e7b68ed9d84c5af6c94897f32
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-11-28 10:32:40 +0530

    Investryx

--- Python/smerg_chat/consumers.py
+++ Python/smerg_chat/consumers.py
@@ -103,7 +103,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             chat.duration = duration
             room.last_msg = encrypt_message("Voice message")
         if attachment:
-            decoded_attachment = base64.b64decode(attachment['data'])
+            attachment_dict = json.loads(attachment_json)
+            base64_data = attachment_dict['data']
+            decoded_attachment = base64.b64decode(base64_data)
             with Image.open(BytesIO(decoded_attachment)) as img:
                 image_type = img.format
             filename = f'attachment_{self.user.username}_{time}.{image_type}'
