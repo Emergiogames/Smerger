7d544cab00b290ff1475c6f3b7902349377cf7b7
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-12-02 16:37:33 +0530

    Investryx

--- Python/smerg_chat/views.py
+++ Python/smerg_chat/views.py
@@ -32,14 +32,20 @@ class Rooms(APIView):
         if request.headers.get('token'):
             exists, user = await check_user(request.headers.get('token'))
             if exists:
-                reciever = await SaleProfiles.objects.aget(id=request.data.get('receiverId')).user
-                image = await SaleProfiles.objects.aget(id=request.data.get('receiverId')).user.image.url if SaleProfiles.objects.get(id=request.data.get('receiverId')).user.image and hasattr(SaleProfiles.objects.get(id=request.data.get('receiverId')).user.image, 'url') else None
-                room_exist = await Room.objects.filter(Q(first_person=user, second_person=reciever) | Q(second_person=user, first_person=reciever)).aexists()
-                if room_exist:
-                    room = await Room.objects.aget(Q(first_person=user, second_person=reciever) | Q(second_person=user, first_person=reciever))
-                    return Response({'status':True, 'name': reciever.name, 'image':image, 'roomId': room.id})
+                reciever = await SaleProfiles.objects.aget(id=request.data.get('receiverId'))
+                recieved_user = await sync_to_async(lambda: reciever.user)()
+                recieved_name = await sync_to_async(lambda: reciever.name)()
+                recieved_image = await sync_to_async(lambda: reciever.image)()
+                if recieved_image:
+                    image = recieved_image.url 
+                else: 
+                    None
+                if await Room.objects.filter(Q(first_person=user, second_person=recieved_user) | Q(second_person=user, first_person=recieved_user)).aexists():
+                    room = await Room.objects.aget(Q(first_person=user, second_person=recieved_user) | Q(second_person=user, first_person=recieved_user))
+                    room_id = await sync_to_async(lambda: room.id)()
+                    return Response({'status':True, 'name': recieved_name, 'image':image, 'roomId': room_id})
                 room = await Room.objects.acreate(first_person=user, second_person=reciever, last_msg=encrypt_message("Tap to send message"))
-                return Response({'status':True,'name': reciever.name, 'image':image, 'roomId':room.id})
+                return Response({'status':True,'name': recieved_name, 'image':image, 'roomId':room.id})
             return Response({'status':False,'message': 'User doesnot exist'})
         return Response({'status':False,'message': 'Token is not passed'})
 
