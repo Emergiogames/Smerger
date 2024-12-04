0b81da18507c04b34121a943ff6fa12bc003bfd6
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2024-12-04 11:35:27 +0530

    Invetsryx

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -808,12 +808,13 @@ class Recommended(APIView):
                         query |= Q(industry__in=preference.industries)
                     # if not request.GET.get('type') and preference.profile:
                     #     query |= Q(entity_type__in=preference.profile)
-                    # if request.GET.get('type') != "advisor":
-                    #     if preference.price_starting is not None:
-                    #         query |= Q(range_starting__gte=preference.price_starting)
-                    #     if preference.price_starting is not None:
-                    #         query |= Q(range_ending__lte=preference.price_ending)
+                    if request.GET.get('type') != "advisor":
+                        if preference.price_starting is not None:
+                            query |= Q(range_starting__gte=preference.price_starting)
+                        if preference.price_starting is not None:
+                            query |= Q(range_ending__lte=preference.price_ending)
                     if request.GET.get('type') or request.GET.get('type') != "advisor":
+                        print("WORKING...!")
                         if request.GET.get('type') != "advisor":
                             query &= Q(entity_type=request.GET.get('type'), verified=True)
                         products = [posts async for posts in SaleProfiles.objects.filter(query).order_by('-id')]
