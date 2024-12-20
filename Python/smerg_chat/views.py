faf0d9dbd0ec0a270f7c8afe5ce25035237f5ef3
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2024-12-20 13:03:38 +0530

    Fix enquiry creation logic in Rooms view to ensure it is always executed

--- Python/smerg_chat/views.py
+++ Python/smerg_chat/views.py
@@ -40,12 +40,12 @@ class Rooms(APIView):
                     image = recieved_image.url 
                 else: 
                     image = None
+                enquiry = await Enquiries.objects.acreate(user=user, post=reciever)
                 if await Room.objects.filter(Q(first_person=user, second_person=recieved_user) | Q(second_person=user, first_person=recieved_user)).aexists():
                     room = await Room.objects.aget(Q(first_person=user, second_person=recieved_user) | Q(second_person=user, first_person=recieved_user))
                     room_id = await sync_to_async(lambda: room.id)()
                     return Response({'status':True, 'name': recieved_name, 'image':image, 'roomId': room_id})
                 room = await Room.objects.acreate(first_person=user, second_person=recieved_user, last_msg=encrypt_message("Tap to send message"))
-                enquiry = await Enquiries.objects.acreate(user=user, post=reciever)
                 return Response({'status':True,'name': recieved_name, 'image':image, 'roomId':room.id})
             return Response({'status':False,'message': 'User doesnot exist'})
         return Response({'status':False,'message': 'Token is not passed'})
