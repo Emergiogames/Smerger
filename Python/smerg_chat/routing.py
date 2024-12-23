c62f03245b0efcbc9492a8a0239a94df5ce8b796
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2024-12-23 18:24:11 +0530

    Format routing.py for consistency in code style

--- Python/smerg_chat/routing.py
+++ Python/smerg_chat/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('<int:id>',consumers.ChatConsumer.as_asgi()),
-    path('rooms',consumers.RoomConsumer.as_asgi()),
-    path('noti_web',consumers.NotiConsumer.as_asgi()),
+    path('<int:id>', consumers.ChatConsumer.as_asgi()),
+    path('rooms', consumers.RoomConsumer.as_asgi()),
+    path('noti_web', consumers.NotiConsumer.as_asgi()),
 ]
