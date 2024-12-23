733ec07e28bdb8483c603e24a206dde9f291a93f
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2024-12-23 17:50:31 +0530

    Rename notification route to noti_web for consistency in routing

--- Python/smerg_chat/routing.py
+++ Python/smerg_chat/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 websocket_urlpatterns = [
     path('<int:id>',consumers.ChatConsumer.as_asgi()),
     path('rooms',consumers.RoomConsumer.as_asgi()),
-    path('notification',consumers.NotiConsumer.as_asgi()),
+    path('noti_web',consumers.NotiConsumer.as_asgi()),
 ]
